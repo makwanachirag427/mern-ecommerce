cde09a13f0294ec1d1fd44e44d2f364f12ced4cc
Chirag Makwana <makwanachirag427@gmail.com>
2025-07-06 18:43:28 +0530

    bulld command added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -3,9 +3,9 @@ import { ENVVARS } from "./utils/envVars";
 import { connectDB } from "./config/db";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
-
-// routes 
+// routes
 import authRoutes from "./routes/auth.route";
 import productRoutes from "./routes/product.route";
 import cartRoutes from "./routes/cart.route";
@@ -19,10 +19,12 @@ const PORT = ENVVARS.PORT || 5000;
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({
-  origin :"http://localhost:5173",
-  credentials: true,
-}))
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
@@ -30,6 +32,14 @@ app.use("/api/cart", cartRoutes);
 app.use("/api/coupons", couponRoutes);
 app.use("/api/payments", paymentRoutes);
 app.use("/api/analytics", analyticsRoutes);
+
+if (ENVVARS.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
