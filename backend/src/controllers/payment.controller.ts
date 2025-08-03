d3a8c8491b3ad92275cced518711fa1fea0d81b3
Chirag Makwana <makwanachirag427@gmail.com>
2025-08-03 10:28:30 +0530

    Shipping address added

--- backend/src/controllers/payment.controller.ts
+++ backend/src/controllers/payment.controller.ts
@@ -5,7 +5,10 @@ import { stripe } from "../config/stripe";
 import { ENVVARS } from "../utils/envVars";
 import Order from "../models/order.model";
 
-const FRONTEND_URL = ENVVARS.NODE_ENV === "developemnt" ? ENVVARS.NODE_ENV : "https://mern-ecommerce-k0ia.onrender.com";
+const FRONTEND_URL =
+  ENVVARS.NODE_ENV === "developemnt"
+    ? ENVVARS.NODE_ENV
+    : "https://mern-ecommerce-k0ia.onrender.com";
 
 export const createCheckoutSession = async (
   req: RequestType,
@@ -58,6 +61,9 @@ export const createCheckoutSession = async (
       mode: "payment",
       success_url: `${FRONTEND_URL}/purchase-success?session_id={CHECKOUT_SESSION_ID}`,
       cancel_url: `${FRONTEND_URL}/purchase-cancel`,
+      shipping_address_collection: {
+        allowed_countries: ["US", "IN"], // Add others as needed
+      },
       discounts: coupon
         ? [
             {
